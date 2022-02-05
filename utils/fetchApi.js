eeb8d7fb7ccadde0e4ec5414d26268ac212e347b
anupam <anupamdas0376@gmail.com>
2022-02-05 23:40:39 +0600

    Security changed

--- utils/fetchApi.js
+++ utils/fetchApi.js
@@ -1,3 +1,4 @@
+require('dotenv').config();
 import axios from 'axios';
 
 export const URL ='https://bayut.p.rapidapi.com'
@@ -6,7 +7,7 @@ export const fetchApi = async(url)=>{
     const {data} = await axios.get((url),{
         headers: {
             'x-rapidapi-host': 'bayut.p.rapidapi.com',
-            'x-rapidapi-key': '7a78fc9141msh93674a2b1d340c6p1bc6fcjsnc33899ec9b14'
+            'x-rapidapi-key': process.env.KEY
           }
     })
 
